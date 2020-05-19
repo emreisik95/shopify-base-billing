e8cbc7df589d2106cf834648bcd42aac6abec08f
emreisik95 <emreisik@yandex.com>
2020-05-20 01:49:16 +0300

    Update shopify_app.rb

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = 'Sticky Checkout Button'
-  config.api_key = ENV["SHOPIFY_STICKY_CHECKOUT_KEY"]
-  config.secret = ENV["SHOPIFY_STICKY_CHECKOUT_SECRET"]
+  config.api_key = ENV["8a5990b767f56bd22c37a1c0a03fc951"]
+  config.secret = ENV["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"]
   config.scope = "read_orders,read_products,read_script_tags,write_script_tags"
   config.embedded_app = true
 end
